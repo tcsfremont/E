e2762e5d188d7d9c45fe560427318136b061435f
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-07-21 15:02:51 -0700

    aliens dropping

--- spaceinvaders/spaceinvaders.py
+++ spaceinvaders/spaceinvaders.py
@@ -1,11 +1,12 @@
 import pgzrun
+from random import random
 player=Actor('ranger')
 player.x=400
 player.y = 600 - player.height * 0.5
 aliens=[]
 def add_alien():
     alien=Actor('slimey')
-    alien.x = 400
+    alien.x = 800 *0.5 + (800 - alien.width) * random()
     alien.y = alien.height * 0.5
     aliens.append(alien)
 clock.schedule_interval(add_alien,5)# every 5 seconds
@@ -20,8 +21,17 @@ def update():
         player.x=min(800 - player.width * 0.5, player.x + 5)
     if keyboard.space:
         fire_laser()
+    for alien in aliens:
+        alien.y += 3
     for laser in lasers:
         laser.y -=10
+        for alien in aliens:
+            if laser.x>alien.x -alien.width *0.5:
+                if laser.y > alien.y-alien.height *0.5:
+                    if laser.x < alien.x + alien.width*0.5:
+                        if laser.y < alien.y + alien.height *0.5:
+                            aliens.remove(alien)
+
 def draw():
     screen.clear()
     player.draw()
